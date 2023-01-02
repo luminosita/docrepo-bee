d054eb1bbe7e4a381b4b9fb0f54a772150e1d489
Semaphore <sem@emisia.net>
2023-01-02 01:47:51 +0100

    Initial Commit

--- cmd/bee/main.go
+++ cmd/bee/main.go
@@ -11,7 +11,7 @@ func commandRoot() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use: "bee",
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
+			_ = cmd.Help()
 			os.Exit(2)
 		},
 	}
@@ -20,9 +20,25 @@ func commandRoot() *cobra.Command {
 	return rootCmd
 }
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
 func main() {
 	if err := commandRoot().Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+		_, _ = fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(2)
 	}
 }
--- cmd/bee/version.go
+++ cmd/bee/version.go
@@ -7,15 +7,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "DEV"
-
 func commandVersion() *cobra.Command {
+	//TODO : Needs to go to config
+	version := "DEV"
+
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print the version and exit",
 		Run: func(_ *cobra.Command, _ []string) {
 			fmt.Printf(
-				"Dex Version: %s\nGo Version: %s\nGo OS/ARCH: %s %s\n",
+				"Bee Version: %s\nGo Version: %s\nGo OS/ARCH: %s %s\n",
 				version,
 				runtime.Version(),
 				runtime.GOOS,
